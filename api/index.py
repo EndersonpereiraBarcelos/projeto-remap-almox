b79570cb4ded22e2682f88f7905b651cd450bcf0
endersonpb <endersonpb0@gmail.com>
2024-08-03 10:18:24 -0300

    update

--- api/index.py
+++ api/index.py
@@ -5,3 +5,24 @@ app = FastAPI()
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+##POST 
+@app.get("/api/requisicao")
+def new_requicao():
+    return {"message": "Nova requisição"}
+
+#GET
+@app.get("/api/criado")
+def criado():
+    return {"message": "Requisição criada"}
+
+#GET
+@app.get("/api/aprovado")
+def aprovado():
+    return {"message": "Requisição aprovada"}
+
+## GetPost
+@app.get("/api/contabilizacao")
+def contabilizacao():
+    return {"message": "Requisição contabilizada"}
+
---
+++ checklist.md
@@ -0,0 +1,12 @@
+## checklist 
+- [x] Estrutura projeto 
+- [ ] Instalar depêndencias 
+- [x] Python3, javascript
+- [ ] Pydantic banco dados
+- [ ] AWS
+- [x] Terraform
+- [ ] Esboço projeto
+- [ ] Melhorias
+- [ ]
+- [ ]
+ 
